4998ca5683fc6759a14ee5d1029a7306ae70f596
Wandy Santana <sntwandy@gmail.com>
2020-11-23 15:01:24 -0400

    Challenge completed

--- js/index.js
+++ js/index.js
@@ -1,8 +1,37 @@
+Vue.component("modal", {
+  props: ["view"],
+  methods: {
+    closeModal() {
+      this.$emit("change-view");
+    },
+  },
+  template: `
+    <div v-bind:style="{display: view}" class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <h3>title</h3>
+          <div>body</div>
+          <footer>
+            <button v-on:click="closeModal">Cerrar</button>
+          </footer>
+        </div>
+      </div>
+    </div>`,
+});
+
 new Vue({
   el: "#root",
   data() {
     return {
-      title: "Hello Vue",
+      view: "",
     };
+  },
+  methods: {
+    openModal() {
+      this.view = "table";
+    },
+    closeModal() {
+      this.view = "none";
+    },
   },
 });
